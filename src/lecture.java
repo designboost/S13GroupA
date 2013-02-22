c080b70f7aa98bf5f5653961820a9f968286bf6c
Bjørn Barfoed <bjornbarfoed@gmail.com>
2013-02-22 12:30:53 +0100

    du er en

--- src/lecture.java
+++ src/lecture.java
@@ -1,4 +1,25 @@
 
-public class lecture {
+public class Lecture {
+
+	private String title;
+	private Ansat lecturer;
+	private String description;
+	private int duration;
+	
+	public Lecture(lecturer, title, duration) {
+		this.title = title;
+		this.duration = duration;
+		this.lecturer = lecturer;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
